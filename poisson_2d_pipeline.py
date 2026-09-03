b59b2983f7a2b994ef09486f8e7e773f272d5494
rennisson <rennisson.alves@gmail.com>
2026-09-02 23:34:24 -0300

    inverte ordem de execucao dos metodos

--- poisson_2d_pipeline.py
+++ poisson_2d_pipeline.py
@@ -22,9 +22,9 @@ def run_script(script_path):
 def main():
     scripts_to_run = [
         # "fem/fem_poisson_1d.py",
-        "pinns/pinn_poisson_2d.py",
         "vpinns/vpinn_2d_poisson.py",
-        "sv-pinns/svpinn_2d_poisson.py"
+        "sv-pinns/svpinn_2d_poisson.py",
+        "pinns/pinn_poisson_2d.py",
     ]
     
     print("Iniciando Pipeline de Execução Poisson 1D...")
